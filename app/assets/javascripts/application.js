17acc072a834d6247bc65aa5059fd7be24e4f1ff
tomoki-music <end-of-dream1969@ae.auone-net.jp>
2022-06-12 03:11:06 +0000

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,3 @@
+//= require jquery
+//= require rails-ujs
+//= require jquery_ujs
---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,24 @@
+class BookCommentsController < ApplicationController
+  def create
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      render :book_comments
+    else
+      render 'book/show'
+    end
+  end
+
+  def destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
+    render :book_comments
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
